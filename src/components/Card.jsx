d79659e1e41f2b5536bf6ef32970d3590cf8c60e
Vedant Anand <VEDANTANAND0077@gmail.com>
2025-01-02 17:26:47 +0530

    basic card

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Card() {
+    return (
+        <div className='h-[80vh] bg- m-auto w-[80vw] rounded-l-3xl bg-black'>
+            <div className='h-[80vh] bg- m-auto w-[80vw] rounded-l-3xl z-50 flex justify-center items-center text-8xl text-white absolute'>
+                hiiiiiiiiiiiiiii
+            </div>
+            <img src="https://media.discordapp.net/attachments/1314526467144814635/1324340684366282792/1000145998.jpg?ex=6777cbe0&is=67767a60&hm=fdfcaf7ecb49d2211ddcff58e32a8b9f5bba997de82025a2eaa20fc0b1ccf326&=&format=webp&width=735&height=735" alt="" className='object-fill w-[80vw] h-[80vh] rounded-l-3xl absolute z-0' />
+            <img src="https://cdn.discordapp.com/attachments/1314526467144814635/1324343797273985034/WhatsApp_Image_2024-12-16_at_19.56.38_b381d835-removebg-preview_5.png?ex=6777cec6&is=67767d46&hm=f30fa15fe7f027bcc9a4c30ea8553b0978ebaf1d15cca5ae1b8b5aaf0286707a&" alt="" className='absolute ml-[1.5rem] z-0' />
+            
+        </div>
+    )
+}
