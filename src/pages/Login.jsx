607c0b0fe91f542b2fb36580b24614003a765776
Vedant Anand <VEDANTANAND0077@gmail.com>
2025-01-02 22:59:18 +0530

    link update

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -65,7 +65,7 @@ export default function Login() {
                             <div className="text-4xl font-bold mb-1">Create new account</div>
                             <div className="flex">
                                 <span className="text-sm">Already have an account? </span>
-                                <Link href="" className="text-sm ml-1 text-blue-600 underline font-semibold hover:text-blue-800">Sign in</Link>
+                                <Link to="/register" className="text-sm ml-1 text-blue-600 underline font-semibold hover:text-blue-800">Register</Link>
                             </div>
                         </div>
                         <form className="w-80 gap-6 flex flex-col font-semibold" onSubmit={handleSubmit}>
