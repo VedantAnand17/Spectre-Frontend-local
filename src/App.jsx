c5cbe36743e15ffbad6402496affcde843d852cb
Vedant Anand <VEDANTANAND0077@gmail.com>
2025-01-03 14:20:50 +0530

    landing responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Register from "./pages/Register"
 import Login from "./pages/Login"
 import Profile from "./pages/Profile"
+import Landing from "./pages/Landing"
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/" element={<Landing />} />
     </Routes>
   )
 }
