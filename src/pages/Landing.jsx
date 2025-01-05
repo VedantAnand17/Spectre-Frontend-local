3d393e1b9d861e3cd4633b6d326168d94dbf82a7
Vedant Anand <VEDANTANAND0077@gmail.com>
2025-01-05 20:48:18 +0530

    Navbar enhance

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -7,14 +7,14 @@ export default function Landing() {
     const { isLoggedIn } = useContext(AuthContext);
 
     return (
-        <div className='max-w-screen'>
+        <div className='w-screen'>
             <img src="https://cdn.discordapp.com/attachments/1314526467144814635/1324651820072829019/Desktop_-_40.png?ex=6778eda4&is=67779c24&hm=7ad13f000d4ec217b872b32701a51f68ceb3c4c7ddda148fff71a3063dc230d6&" className='h-screen w-screen absolute object-cover' alt="" />
-            <div className="h-screen max-w-screen absolute flex max-lg:justify-center">
+            <div className="h-screen w-screen absolute flex max-lg:justify-center">
                 <img src="https://cdn.discordapp.com/attachments/1314526467144814635/1324343797273985034/WhatsApp_Image_2024-12-16_at_19.56.38_b381d835-removebg-preview_5.png?ex=67787786&is=67772606&hm=fcfcc4128798458f59642d22bbeab7c250315ed4a1813f89f3bdacfb6cd71d31&" className='absolute ml-6 flex justify-center  ' alt="" />
             </div>
-            <div className="h-screen max-w-screen absolute text-white">
+            <div className="h-screen w-screen absolute text-white">
                 <div className="flex lg:gap-60 lg:justify-around items-center h-screen text-xl max-lg:flex-col max-lg:justify-end ">
-                    <div className="max-lg:flex-col gap-60 justify-around lg:min-w-screen flex ml-8 max-lg:gap-10 pb-4 items-center">
+                    <div className="max-lg:flex-col gap-60 justify-around lg:w-screen flex ml-8 max-lg:gap-10 pb-4 items-center">
                         {/* <Button className="bg-[#4F4D4D] p-6 rounded-3xl uppercase hover:bg-[#333232] text-xl font-bold">Register</Button> */}
                         {!isLoggedIn && <>
                             <Link to="/register">
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -111,7 +111,7 @@ export default function Profile() {
 
             <img src="https://cdn.discordapp.com/attachments/1314526467144814635/1324343797273985034/WhatsApp_Image_2024-12-16_at_19.56.38_b381d835-removebg-preview_5.png?ex=6777cec6&is=67767d46&hm=f30fa15fe7f027bcc9a4c30ea8553b0978ebaf1d15cca5ae1b8b5aaf0286707a&" className='absolute ml-6 max-md:h-14' alt="" />
 
-            <div className="h-screen max-w-screen flex justify-center items-center">
+            <div className="h-screen max-w-screen flex justify-center items-center ">
                 <div className="bg-[#030925] bg-opacity-80 h-[80vh] w-[80vw] rounded-3xl m-auto absolute">
                     <img src="https://cdn.discordapp.com/attachments/1314526467144814635/1324475206554878106/image_144.png?ex=67784928&is=6776f7a8&hm=37591d94f44758dbb593aaf67b5655afe0350db26257c1b81af2820f37e644ef&" className='absolute h-[80vh] w-[80vw]' alt="" />
                     {/* <img src="https://cdn.discordapp.com/attachments/1314526467144814635/1324485243419365478/Group_63.png?ex=67785281&is=67770101&hm=34b859a2239301c06f11d8947b55972bcf93a81cf3c86c8b1075e20eebd34d23&" className='absolute max-lg:h-64 max-lg:ml-2 max-sm:h-28 max-lg:mt-24 max-sm:mt-16 ' alt="" /> */}
@@ -119,25 +119,25 @@ export default function Profile() {
                     <img src="https://cdn.discordapp.com/attachments/1314526467144814635/1324475555856515205/Ellipse_28.png?ex=6778497c&is=6776f7fc&hm=b6e6ae1edbad704e518fa3b971d9daa307b9faddcaa5539b153ad9c18ac37955&" className='absolute lg:pl-14 lg:pt-12 max-lg:w-32 max-lg:h-32 pl-10' alt="" /> */}
                     <div className="absolute text-6xl max-lg:text-4xl w-[80vw] text-white flex justify-center font-semibold">PROFILE</div>
                     <div className="flex flex-col items-center gap-10 rounded-3xl bg-opacity-80 max-lg:text-lg h-[80vh] justify-center text-white text-4xl" >
-                        <div className="flex flex-col gap-4">
-                            {!user.verified && <div className='text-sm text-red-500'>Please verify your account to create or join a team.</div>}
+                        <div className="flex flex-col gap-4 max-sm:text-sm font-semibold max-sm:w-52 pl-1">
+                            {!user.verified && <div className='text-sm text-red-500 '>Please verify your account to create or join a team.</div>}
                             {user.username && <div className="">Username: {user.username}</div>}
                             {user.email && <div className="">Registered Mail ID: {user.email}</div>}
-                            {user.collegeName && <div className="">Collage Name: {user.collegeName}</div>}
-                            {user.thaparEmail && <div className="">Collage Mail ID: {user.thaparEmail}</div>}
+                            {user.collegeName && <div className="">College Name: {user.collegeName}</div>}
+                            {user.thaparEmail && <div className="">College Mail ID: {user.thaparEmail}</div>}
                             {user.rollNo && <div className="">Collage Roll NUmber: {user.rollNo}</div>}
                             {user.year && <div className="">Year: {user.year}</div>}
                             {user.phoneNumber && <div className="">Phone Number: {user.phoneNumber}</div>}
                             {user.teamName && <div className="">TeamName: {user.teamName}</div>}
                         </div>
                     </div>
-                    <div className="flex justify-center w-full">
+                    <div className="flex justify-between w-full">
                         {team && user.rollNo == team.leaderRollNo && <Button onClick={() => handleDeleteTeam()} className='absolute bottom-4 left-10 bg-blue-700 hover:bg-blue-800 rounded-xl'>Delete team</Button>}
-                        <Button onClick={() => setIsEditModalOpen(true)} className='absolute bottom-4 bg-blue-700 hover:bg-blue-800 rounded-xl'>Edit Profile</Button>
+                        <Button onClick={() => setIsEditModalOpen(true)} className='absolute left-2 bottom-4 text-white bg-blue-700 hover:bg-blue-800 rounded-xl'>Edit Profile</Button>
                         <Button
                             title='Verify Your Self To Create or Join a Team'
                             onClick={sendVerificationToken}
-                            className="absolute bottom-4 right-10 bg-blue-700 hover:bg-blue-800 rounded-xl"
+                            className="absolute bottom-4 right-2 bg-blue-700 hover:bg-blue-800 text-white rounded-xl"
                             disabled={user.verified || loading}
                         >
                             {loading ? 'Sending...' : user.verified ? 'Verified' : 'Verify Profile'}
